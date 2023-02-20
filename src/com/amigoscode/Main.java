1afdde21466d4133f2dd5ac07c4ffd3c5c0d8bdf
EricRubio589 <eric.rubio.services@gmail.com>
2023-02-20 10:48:38 -0600

    First commit

---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,11 @@
+package com.amigoscode;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int car = 25;
+        System.out.println("Hello Java UwU");
+        System.out.println(10+10);
+        System.out.println(car);
+    }
+}
