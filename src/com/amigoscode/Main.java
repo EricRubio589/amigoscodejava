26c607c2da7ec33857f1d698956652449e707857
EricRubio589 <eric.rubio.services@gmail.com>
2023-03-24 15:40:27 -0500

    Practcing with the new update

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         int car = 25;
+        int bus = 5;
+        bus %= 2;
+        System.out.println(bus);
         System.out.println("Hello Java UwU");
         System.out.println(10+10);
         System.out.println(car);
         System.out.println("new thing");
         //-- This is a comment--//
+        System.out.println( 10 % 5);
+        System.out.println(++car);
     }
 
 }
