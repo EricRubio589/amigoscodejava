7904af1ad03d2c972bf0d886e20c85917bbafb73
EricRubio589 <eric.rubio.services@gmail.com>
2023-05-15 03:45:08 -0500

    Picking up after last

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -3,23 +3,11 @@ package com.amigoscode;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-            Write a program that compares two numbers. i.e x > y
-            Create 3 variables.
-            2 to store both numbers and 1 to store the result
-            Print the result
-        */
-
-        int x = 20;
-        int y = 10;
-        boolean result;
-
-        if (x > y) {
-            result = true;
-        } else {
-            result = false;
+        int i = 0;
+        while(i < 100) {
+            System.out.println("working" + i);
+            i++;
         }
-        System.out.println(result);
 
     }
 
