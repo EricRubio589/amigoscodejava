081f9c809c29fa56cecf6a052d16614a7479b7b3
EricRubio589 <eric.rubio.services@gmail.com>
2023-05-15 21:25:52 -0500

    sum of double that started as string

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,14 +1,24 @@
 package com.amigoscode;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int i = 0;
-        while(i < 100) {
-            System.out.println("working" + i);
-            i++;
+        String numbers = "0.90, 1.00, 9.00, 8.78, 0.01";
+
+        double result = 0;
+
+        String[] perro = numbers.split(", ");
+
+        for (int i = 0; i < perro.length; i++) {
+            double temp1 = Double.parseDouble(perro[i]);
+            result += temp1;
         }
 
+        System.out.println(Arrays.toString(perro));
+        System.out.println(result);
     }
 
 }
